00199ea735aa172e58639b105569c25de7ed7302
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-04-29 00:22:12 +0300

    update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
-import user from 'user.json';
 import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
 
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data.json';
 
 export const App = () => {
   return (
@@ -12,6 +14,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
