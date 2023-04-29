ac9637bf2265bf630f91092c95ff744701eaeac5
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-04-29 19:12:32 +0300

    add propTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -34,11 +34,5 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.arrayOf(
-    PropTypes.shape({
-      followers: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
-    }).isRequired
-  ),
+  stats: PropTypes.object.isRequired,
 };
