d043958b842b7433607ce13f26b3f4a497889bf1
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-04-30 11:57:36 +0300

    Add transactions

---
+++ src/components/Transactions/TransactionHistory.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <div className="container">
+      <table className={css.table}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {items.map(item => (
+            <tr key={item.id}>
+              <td>{item.type}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+TransactionHistory.propType = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
