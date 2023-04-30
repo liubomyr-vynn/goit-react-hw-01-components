489199e583a8cc16dfe111573152405e8d712329
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-04-30 11:58:39 +0300

    Update

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -4,16 +4,18 @@ import { FriendListItem } from './FriendListItem';
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul className={css.list}>
-      {friends.map(friend => (
-        <FriendListItem
-          key={friend.id}
-          avatar={friend.avatar}
-          name={friend.name}
-          isOnline={friend.isOnline}
-        />
-      ))}
-    </ul>
+    <div className="container">
+      <ul className={css.list}>
+        {friends.map(friend => (
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,24 +4,26 @@ import PropTypes from 'prop-types';
 
 export const Statistics = ({ title, stats }) => {
   return (
-    <section className={css.statistics}>
-      {title && <h2 className={css.title}>{title}</h2>}
+    <div className="container">
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
 
-      <ul className={css.statList}>
-        {stats.map(statsItem => (
-          <li
-            className={css.item}
-            key={statsItem.id}
-            style={{
-              backgroundColor: getRandomHexColor(),
-            }}
-          >
-            <span className={css.label}>{statsItem.label}</span>
-            <span className={css.percentage}>{statsItem.percentage}%</span>
-          </li>
-        ))}
-      </ul>
-    </section>
+        <ul className={css.statList}>
+          {stats.map(statsItem => (
+            <li
+              className={css.item}
+              key={statsItem.id}
+              style={{
+                backgroundColor: getRandomHexColor(),
+              }}
+            >
+              <span className={css.label}>{statsItem.label}</span>
+              <span className={css.percentage}>{statsItem.percentage}%</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
   );
 };
 
